c31594f4c7aaa431ae1a5f7f760865bff5583d5b
Marius Koppe <mariuskoppe@gmail.com>
2022-11-20 20:32:50 +0100

    refactoring

---
+++ js/map.js
@@ -0,0 +1,141 @@
+let map;
+let layerControl;
+let modelViewer;
+let checkbox;
+
+function initMap() {
+  // set minZoom to 6 in order to prevent whole world zoom
+  map = L.map("map", {
+    minZoom: 6,
+  }).setView([50.0348485, 8.2406363], 11);
+
+  map.fitBounds([
+    [49.3948229196, 7.7731704009],
+    [51.6540496066, 10.2340156149],
+  ]);
+}
+
+function initMapControls() {
+  // Layer Control
+
+  var osmTileLayer = L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  ).addTo(map);
+
+  const resizeObserver = new ResizeObserver(() => {
+    map.invalidateSize();
+  });
+  const mapDiv = document.getElementById("map");
+
+  resizeObserver.observe(mapDiv);
+
+  // https://www.geoportal.hessen.de/mapbender/php/wms.php?inspire=1&layer_id=37745&withChilds=1&REQUEST=GetCapabilities&SERVICE=WMS
+  var hessen_umring_wms = L.tileLayer.wms(
+    "https://sgx.geodatenzentrum.de/wms_vg1000?",
+    {
+      layers: "vg1000_lan",
+      format: "image/png",
+      transparent: true,
+      attribution:
+        "Hessisches Landesamt für Bodenmanagement und Geoinformation",
+    }
+  );
+
+  var hessen_rbz_wms = L.tileLayer.wms(
+    "https://sgx.geodatenzentrum.de/wms_vg1000?",
+    {
+      layers: "vg1000_rbz",
+      format: "image/png",
+      transparent: true,
+      attribution:
+        "Hessisches Landesamt für Bodenmanagement und Geoinformation",
+    }
+  );
+
+  var wms_dtk = L.tileLayer.wms(
+    "https://sgx.geodatenzentrum.de/wms_dtk_klein?",
+    {
+      layers: "dtk",
+      format: "image/png",
+      transparent: true,
+      attribution:
+        "Dienstleistungszentrum des Bundes für Geoinformation und Geodäsie",
+    }
+  );
+
+  // legend URL https://services.bgr.de/wms/geologie/gk1000/?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=0
+  var wms_geologie = L.tileLayer.wms(
+    "https://services.bgr.de/wms/geologie/gk1000/?",
+    {
+      layers: "0",
+      format: "image/png",
+      transparent: true,
+      attribution: "Bundesanstalt für Geowissenschaften und Rohstoffe (BGR)",
+    }
+  );
+
+  var baseLayers = {
+    OpenStreetMap: osmTileLayer,
+    "Digitale Topographische Karte": wms_dtk,
+  };
+
+  var overlays = {
+    "Geologische Karte": wms_geologie,
+    "Landesgrenze Hessen": hessen_umring_wms,
+    "Regierungsbezirke Hessen": hessen_rbz_wms,
+  };
+
+  layerControl = L.control.layers(baseLayers, overlays).addTo(map);
+
+  // Scale Control
+  L.control
+    .scale({
+      maxWidth: 500,
+      metric: true,
+      imperial: false,
+    })
+    .addTo(map);
+
+  // static content for attribution control
+  let attributionControl = map.attributionControl;
+  attributionControl.setPrefix("HLNUG");
+}
+
+function onEachFeature_asTable(feature, layer) {
+  if (feature.properties) {
+    let html =
+      "<div class='table-responsive'><table class='table table-striped table-sm'>";
+
+    html += "<thead>";
+
+    html += "<tr>";
+    html += "<th><b>Attributname</b></th>";
+    html += "<th><b>Wert</b></th>";
+    html += "</tr>";
+
+    html += "</thead>";
+    html += "<tbody>";
+
+    for (const key in feature.properties) {
+      if (Object.hasOwnProperty.call(feature.properties, key)) {
+        const element = feature.properties[key];
+        html += "<tr>";
+        html += "<td>" + key + "</td>";
+        html += "<td>" + element + "</td>";
+        html += "</tr>";
+      }
+    }
+
+    html += "</tbody>";
+    html += "</table></div>";
+
+    layer.bindPopup(html);
+  } else {
+    layer.bindPopup("No properties found");
+  }
+}
+
--- js/steckbrief.js
+++ js/steckbrief.js
@@ -1,30 +1,29 @@
-fetchProfile();
-const div = document.getElementById('steckbrief');
+const div = document.getElementById("steckbrief");
 console.log("div: " + div);
 
 function fetchProfile() {
-    console.log("Steckbriefe");
-        // fetch-Aufruf mit Pfad zur XML-Datei
-    fetch ('./steckbriefe/steckbrief.xml')
-    .then (function (response) {
-        // Antwort kommt als Text-String
-        return response.text();
+  console.log("Steckbriefe");
+  // fetch-Aufruf mit Pfad zur XML-Datei
+  fetch("./steckbriefe/steckbrief.xml")
+    .then(function (response) {
+      // Antwort kommt als Text-String
+      return response.text();
     })
-    .then (function (data) {
-        console.log (data);			  // schnell mal in der Konsole checken
-        
-        // String in ein XML-DOM-Objekt umwandeln
-        let parser = new DOMParser (),
-            xmlDoc = parser.parseFromString (data, 'text/xml');
-        
-        //und noch ein paar Test-Ausgaben in die Konsole
-        console.log (xmlDoc.getElementsByTagName ('item'));
-        //console.log ("item "  + xmlDoc.getElementsByTagName ('item')[1].children[0].textContent);        
-        console.log (xmlDoc);
-        //alert(div);
-        div.innerHTML = data;
-    }).catch (function (error) {
-        console.log ("Fehler: bei Auslesen der XML-Datei " + error);
+    .then(function (data) {
+      console.log(data); // schnell mal in der Konsole checken
+
+      // String in ein XML-DOM-Objekt umwandeln
+      let parser = new DOMParser(),
+        xmlDoc = parser.parseFromString(data, "text/xml");
+
+      //und noch ein paar Test-Ausgaben in die Konsole
+      console.log(xmlDoc.getElementsByTagName("item"));
+      //console.log ("item "  + xmlDoc.getElementsByTagName ('item')[1].children[0].textContent);
+      console.log(xmlDoc);
+      //alert(div);
+      div.innerHTML = data;
+    })
+    .catch(function (error) {
+      console.log("Fehler: bei Auslesen der XML-Datei " + error);
     });
 }
-
